02448fb18d2204351374700b5fde375a77492d90
sonu-96 <sonuy0199@gmail.com>
2025-10-07 15:19:04 +0530

    test

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const version = process.env.APP_VERSION || 'v0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end(`Hello from POC successfull on test app - ${version}\n`);
+  res.end(`Hello from POC success test app - ${version}\n`);
 });
 
 server.listen(port, () => {
